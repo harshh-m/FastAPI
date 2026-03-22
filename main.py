13dade850ddfabd120793854377ca128dd606703
Harsh Azad Mishra <hm972555@gmail.com>
2026-03-22 21:22:42 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI
+from models import Product
 
-app= FastAPI()
+app = FastAPI()
+
 @app.get("/")
 def root():
-    return{"message":"Hello World"}
+    return {"message": "Hello World"}
+
+products = [
+    Product(id=1, name="Mobile", price=500.99),
+    Product(id=2, name="Smart Watch", price=200.99),
+    Product(id=3, name="Laptop", price=999.9),
+]
+
+@app.get("/products")
+def get_all_products():
+    return products
+
+@app.get("/products/{id}")
+def get_products_by_id(id: int):
+    return products[id-1]
